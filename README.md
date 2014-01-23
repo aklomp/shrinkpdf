5b5271e0c3d42577af811e35c3aa31f039abe77f
Alfred Klomp <git@alfredklomp.com>
2014-01-23 21:26:00 +0100

    Expand the script and the description
    Expand the script to take a commandline argument for the output file,
    but default to stdout. Update the description accordingly. Add some
    bells and whistles to the script to make it more user friendly, such as
    printing a help text and checking whether the shrunken output is
    actually smaller than the input.

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # Shrinkpdf: shrink PDF files with Ghostscript
 
-The simplest shell script in the world to shrink your PDFs (as in reduce
-filesize) under Linux with Ghostscript. Inspired by some code I found in an
-OpenOffice Python script (I think). It feeds an existing PDF through
-Ghostscript, downsamples the images to 72dpi, and does some other stuff. Pretty
-straightforward.
+A simple wrapper around Ghostscript to shrink PDFs (as in reduce filesize)
+under Linux. Inspired by some code I found in an OpenOffice Python script (I
+think). The script feeds a PDF through Ghostscript, which performs lossy
+recompression by such methods as downsampling the images to 72dpi. The result
+should be (but not always is) a much smaller file.
 
 ## Usage
 
-Download the script by clicking the filename at the top of the box. Then run:
+Download the script by clicking the filename at the top of the box. Make it
+executable. If you run it with no arguments, it prints a usage summary. If you
+run it with a single argument -- the name of the pdf to shrink -- it writes the
+result to `stdout`:
 
 ```sh
-# sh shrinkpdf.sh yourfile.pdf
+./shrinkpdf.sh in.pdf > out.pdf
 ```
 
-This produces a shrunken file named `out.pdf` in the current directory.
+You can also provide a second filename for the output:
+
+```sh
+./shrinkpdf.sh in.pdf out.pdf
+```
+
+If both the input and the output are regular files, the script checks if the
+output is actually smaller. If not, it writes a message to `stderr` and copies
+the input over the output.
 
 Sorry, Windows users; this one is Linux only. A Windows adaptation of this
 script can be found [on this blog](http://dcm684.us/wp/2013/10/pdf-shrink/).
--- shrinkpdf.sh
+++ shrinkpdf.sh
@@ -1,16 +1,61 @@
 #!/bin/sh
 
-gs	-q -dNOPAUSE -dBATCH -dSAFER \
-	-sDEVICE=pdfwrite \
-	-dCompatibilityLevel=1.3 \
-	-dPDFSETTINGS=/screen \
-	-dEmbedAllFonts=true \
-	-dSubsetFonts=true \
-	-dColorImageDownsampleType=/Bicubic \
-	-dColorImageResolution=72 \
-	-dGrayImageDownsampleType=/Bicubic \
-	-dGrayImageResolution=72 \
-	-dMonoImageDownsampleType=/Bicubic \
-	-dMonoImageResolution=72 \
-	-sOutputFile=out.pdf \
-	"$1"
+shrink ()
+{
+	gs					\
+	  -q -dNOPAUSE -dBATCH -dSAFER		\
+	  -sDEVICE=pdfwrite			\
+	  -dCompatibilityLevel=1.3		\
+	  -dPDFSETTINGS=/screen			\
+	  -dEmbedAllFonts=true			\
+	  -dSubsetFonts=true			\
+	  -dColorImageDownsampleType=/Bicubic	\
+	  -dColorImageResolution=72		\
+	  -dGrayImageDownsampleType=/Bicubic	\
+	  -dGrayImageResolution=72		\
+	  -dMonoImageDownsampleType=/Bicubic	\
+	  -dMonoImageResolution=72		\
+	  -sOutputFile="$2"			\
+	  "$1"
+}
+
+check_smaller ()
+{
+	# If $1 and $2 are regular files, we can compare file sizes to
+	# see if we succeeded in shrinking. If not, we copy $1 over $2:
+	if [ ! -f "$1" -o ! -f "$2" ]; then
+		return 0;
+	fi
+	ISIZE="$(wc -c "$1" | cut -f1 -d\ )"
+	OSIZE="$(wc -c "$2" | cut -f1 -d\ )"
+	if [ "$ISIZE" -lt "$OSIZE" ]; then
+		echo "Input smaller than output, doing straight copy" >&2
+		cp "$1" "$2"
+	fi
+}
+
+usage ()
+{
+	echo "Reduces PDF filesize by lossy recompressing with Ghostscript."
+	echo "Not guaranteed to succeed, but usually works."
+	echo "  Usage: $1 infile [outfile]"
+}
+
+IFILE="$1"
+
+# Need an input file:
+if [ -z "$IFILE" ]; then
+	usage "$0"
+	exit 1
+fi
+
+# Output filename defaults to "-" (stdout) unless given:
+if [ ! -z "$2" ]; then
+	OFILE="$2"
+else
+	OFILE="-"
+fi
+
+shrink "$IFILE" "$OFILE" || exit $?
+
+check_smaller "$IFILE" "$OFILE"
